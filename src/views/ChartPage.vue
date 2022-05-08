b318d4a960d21002ca61d5d4da6917ce55a7ee2e
Moises <mhdezcoronado@gmail.com>
2022-05-08 16:36:15 +0200

    Create ChartPage & TablePage

---
+++ src/views/ChartPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="chart">
+    <h1>Chart</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChartPage',
+}
+</script>
---
+++ src/views/TablePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="table">
+    <h1>Table</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TablePage',
+}
+</script>
