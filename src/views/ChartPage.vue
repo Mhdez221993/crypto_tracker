1e7e315f399030ccbe71443ae6c9757dbe8dc9ea
Moises <mhdezcoronado@gmail.com>
2022-05-09 01:37:59 +0200

    Applay draggable method

--- src/views/ChartPage.vue
+++ src/views/ChartPage.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="chart">
     <h1>Chart</h1>
+
+    <div id="mydiv" v-on:click="dragElement">
+      <div id="mydivheader">Click here to move</div>
+    </div>
+
   </div>
 </template>
 
 <script>
+
+import dragger from '../helpers/dragger'
+
 export default {
   name: 'ChartPage',
+
+  methods: {
+    dragElement: dragger
+  }
 }
+
 </script>
