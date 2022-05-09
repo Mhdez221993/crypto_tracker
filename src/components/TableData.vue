81dd6de01d02ecac2dfbe4525a356a22537b6280
Moises <mhdezcoronado@gmail.com>
2022-05-10 01:10:35 +0200

    Fetch data with axios

--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -2,16 +2,21 @@
   <v-card>
     <v-card-title>
       Nutrition
+
       <v-spacer></v-spacer>
       <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+
     </v-card-title>
     <v-data-table :headers="headers" :items="data" :search="search"></v-data-table>
+
   </v-card>
 </template>
 
 <script>
 
+import updateData from '@/helpers/converData'
 import { mockTableData } from '@/helpers/mockData'
+import axios from 'axios'
 
 export default {
   name: 'TableData',
@@ -27,8 +32,14 @@ export default {
           sortable: false,
           value: 'name'
         },
-        { text: 'Date', value: 'date' },
-        { text: 'Price', value: 'price' }
+        {
+          text: 'Date',
+          value: 'date'
+        },
+        {
+          text: 'Price',
+          value: 'price'
+        }
       ],
       data: mockTableData
     }
@@ -37,11 +48,30 @@ export default {
     toggleOrder() {
       this.sortDesc = !this.sortDesc
     },
+
     nextSort() {
       let index = this.headers.findIndex(h => h.value === this.sortBy)
+
       index = (index + 1) % this.headers.length
+
       this.sortBy = this.headers[index].value
     },
+  },
+
+  async mounted() {
+    this.loaded = false
+
+    try {
+      const { data } = await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2019-12-31')
+
+
+      this.data = updateData(data)
+
+      this.loaded = true
+
+    } catch (e) {
+      console.error(e)
+    }
   },
 }
 </script>
